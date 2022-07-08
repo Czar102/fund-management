a0ba7665a388b9ad7f75fbe73f7be926a4f707cf
Czar102 <79329479+Czar102@users.noreply.github.com>
2022-07-08 18:33:22 +0200

    cleanup

--- test/mocks/ForceETHTransfer.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.13;
-
-contract ForceETHTransfer {
-	constructor(address to) payable {
-		require(msg.value != 0, "transferring zero value");
-		selfdestruct(payable(to));
-	}
-}
